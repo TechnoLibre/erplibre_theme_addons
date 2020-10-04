e15720761db38f2c917e4fb05f7ae6f43787b730
Carms Ng <carmenhyng89@gmail.com>
2020-10-04 19:33:31 -0400

    [ADD] New module for email template

---
+++ email_template_custom_erplibre/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'email template custom erplibre',
+    'author': 'TechnoLibre',
+    'website': 'https://technolibre.ca',
+    'category': 'Other',
+    'description': """
+email template custom erplibre
+================================
+
+    """,
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'data/mail_data.xml',
+    ],
+    "auto_install": False,
+}
